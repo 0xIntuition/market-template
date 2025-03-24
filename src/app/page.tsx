9cdba6c733b8f1f2c85bbdb476d0c07d9f0c850e
Auroter <seangherardi@gmail.com>
2025-03-24 10:56:10 -0700

    force dynamic root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { getEntries } from '@/server/entries'
 import { EntryFeed } from '@/components/EntryFeed'
 import type { EntryListType } from '@/server/entries'
